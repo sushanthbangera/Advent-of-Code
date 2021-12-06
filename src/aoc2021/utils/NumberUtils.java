20cde994624439265e3e892578288a377a4cced1
Sushanth Bangera <sushanth.bangera@netcracker.com>
2021-12-06 16:58:48 +0530

    Added Utils for reading comma separated String

--- src/aoc2021/utils/NumberUtils.java
+++ src/aoc2021/utils/NumberUtils.java
@@ -1,14 +1,18 @@
 package aoc2021.utils;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class NumberUtils {
 
     public static List<Integer> convertToInt(List<String> input) {
-        return input.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        return input.stream().map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    public static List<Integer> convertToInt(String input, String splitRegex) {
+        String[] inputArray = input.split(splitRegex);
+        return Arrays.stream(inputArray).map(Integer::parseInt).collect(Collectors.toList());
     }
 
 }
