497114c4fd53379705007bdac6725a82b3828b99
Sushanth Bangera <sushanth.bangera@netcracker.com>
2021-12-09 10:08:41 +0530

    Adding solution for Day 8

---
+++ src/aoc2021/Day8/SevenSegmentSearch.java
@@ -0,0 +1,36 @@
+package aoc2021.Day8;
+
+import aoc2020.utils.FileUtils;
+
+import java.util.List;
+
+public class SevenSegmentSearch {
+
+    protected static String inputPath1 = "C:\\MyDrive\\Study\\My_Github_Repo\\Advent-of-Code\\src\\aoc2021\\Day8\\input1";
+    protected static String inputPath2 = "C:\\MyDrive\\Study\\My_Github_Repo\\Advent-of-Code\\src\\aoc2021\\Day8\\input2";
+
+    public static void main(String[] args) {
+        List<String> input1 = FileUtils.readInputFile(inputPath1);
+        List<String> input2 = FileUtils.readInputFile(inputPath2);
+
+        System.out.println(getSolution(input1));
+        System.out.println(getSolution(input2));
+    }
+
+    private static int getSolution(List<String> input) {
+        int result = 0;
+        for (String in : input) {
+            System.out.println(in);
+            String ch[] = in.split("\\s+");
+
+            for (int i = 11; i < ch.length; i++) {
+                if (ch[i].length() == 2 || ch[i].length() == 3 || ch[i].length() == 4 || ch[i].length() == 7) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+
+}
