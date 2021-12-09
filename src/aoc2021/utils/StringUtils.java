702466e86f8b25497d6a9878c54d43e3a2662412
Sushanth Bangera <sushanth.bangera@netcracker.com>
2021-12-09 10:16:34 +0530

    added utils for sorting string

--- src/aoc2021/utils/StringUtils.java
+++ src/aoc2021/utils/StringUtils.java
@@ -1,8 +1,16 @@
 package aoc2021.utils;
 
+import java.util.Arrays;
+
 public class StringUtils {
 
     public static String[] split(String input) {
         return input.split("\\s+");
+    }
+
+    public static String sortString(String inputString) {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
     }
 }
